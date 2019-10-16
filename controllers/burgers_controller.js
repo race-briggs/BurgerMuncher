377cfef4bf923c666a841db833101b9c56f0ba80
race-briggs <race.briggs@gmail.com>
2019-10-16 15:10:08 -0500

    added router endpoint burger functions

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,15 +5,39 @@ var router = express.Router();
 var burger = require('../models/burger');
 
 router.get('/', function(req, res){
-
+  burger.all(function(data){
+    var resObject = {
+      burgers: data
+    };
+    console.log(resObject);
+    res.render('index', resObject);
+  })
 })
 
-router.post('/', function(req, res){
-
+router.post('/api/burgers', function(req, res){
+  burger.create([
+    'burger_name', 'devoured'
+  ], [
+    req.body.burger_name, req.body.devoured
+  ], function(result){
+    res.json({id: result.insertId})
+  });
 })
 
-router.put('/', function(req, res){
+router.put('/api/burgers/:id', function(req, res){
+  var condition = 'id=' + req.params.id;
 
-})
+  console.log(condition);
+
+  burger.update({
+    devoured: req.body.devoured
+  }, condition, function(response){
+    if(response.changedRows == 0){
+      return res.status(404).end();
+    } else{
+      res.status(200).end();
+    }
+  });
+});
 
 module.exports = router;
