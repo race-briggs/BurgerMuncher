ed4bf887337456545c7baca87c04b7c6ee141dcb
race-briggs <race.briggs@gmail.com>
2019-10-16 13:31:18 -0500

    added router and server files

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+var router = express.Router();
+
+var burger = require('../models/burger');
+
+router.get('/', function(req, res){
+
+})
+
+router.post('/', function(req, res){
+
+})
+
+router.put('/', function(req, res){
+
+})
+
+module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,21 @@
 var orm = require('../config/orm');
 
+var burger = {
+  all: function(callBack){
+    orm.selectAll('burgers', function(res){
+      callBack(res);
+    })
+  },
+  create: function(col, val, callBack){
+    orm.insertOne('burgers', col, val, function(res){
+      callBack(res);
+    })
+  },
+  update: function(updateObj, condition, callBack){
+    orm.updateOne('burgers', updateObj, condition, function(res){
+      callBack(res);
+    })
+  }
+}
+
+module.exports = burger;
