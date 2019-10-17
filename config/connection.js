a0d658d2ade7d6f4b7c2ef232cda8ffbe7a4f6cf
race-briggs <race.briggs@gmail.com>
2019-10-17 13:37:16 -0500

    jawsdb shite

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,19 @@ require('dotenv').config();
 
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+var connection;
+
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else{
+  connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   port: 3306,
   password: process.env.DATABASE_PW,
   database: 'burgers_db'
 })
+}
 
 connection.connect(function(error){
   if(error){
